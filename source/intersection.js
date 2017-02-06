cd1f1697586b73ea742e7d7f68c5e9ef86747ea9
denis <dondiego4697@mail.ru>
2017-02-06 23:49:12 +0300

    Вариант-5 Денис Степанов

---
+++ source/intersection.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/*Метод заключается в том, что берется первый входящий массив(resultArr) и все последующие сравниваются с ним.
+* По мере сравнения из массива resultArr удаляются непересекающиеся элементы.*/
+
+const intersection = function () {
+    var arrays = Array.from(arguments);
+    console.log(arrays);
+
+    var resultArr = arrays[0];
+    if (resultArr.length <= 1) {
+        return resultArr;
+    } else {
+        for (var i = 1; i < arrays.length; i++) {
+            let currArr = arrays[i];
+            var j = 0;
+            while (j < resultArr.length) {
+                var resultItem = resultArr[j];
+                var findCheck = false;
+                for (var k = 0; k < currArr.length; k++) {
+                    console.log(resultItem + " === " + currArr[k] + " = " + (resultItem === currArr[k]));
+                    if (resultItem === currArr[k] || (isNaN(resultItem) && isNaN(currArr[k]))) {
+                        findCheck = true;
+                        currArr.splice(k, 1);
+                        break;
+                    } else {
+
+                    }
+                }
+                if (!findCheck) {
+                    resultArr.splice(j, 1);
+                } else {
+                    j++;
+                }
+            }
+        }
+    }
+    return resultArr;
+};
