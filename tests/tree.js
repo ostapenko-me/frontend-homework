969955877a801ea38367157cf7247fe1ffc7a4bd
TeeNik <teenik3315@gmail.com>
2017-02-06 23:44:33 +0300

    Add files via upload

--- tests/tree.js
+++ tests/tree.js
@@ -53,4 +53,20 @@ QUnit.module('Тестируем функцию tree', function () {
 		assert.strictEqual(tree(8), expected);
 		assert.strictEqual(tree('8'), expected);
 	});
+    
+    QUnit.test('Ёлочка высотой 10', function (assert) {
+		const expected =
+			'        *        \n' +
+ 			'       ***       \n' +
+ 			'      *****      \n' +
+ 			'     *******     \n' +
+ 			'    *********    \n' +
+ 			'   ***********   \n' +
+ 			'  *************  \n' +
+ 			' *************** \n' +
+ 			'*****************\n' +
+ 			'        |        \n' ;
+		assert.strictEqual(tree(10), expected);
+		assert.strictEqual(tree('10'), expected);
+	});
 });
