84215db61578828df1da529156683040c5ee807d
XfortunaX <sergbuk96@gmail.com>
2017-02-07 19:41:17 +0300

    Буклин Сергей вариант 5

--- source/intersection.js
+++ source/intersection.js
@@ -1,23 +1,24 @@
 'use strict';
 
 const intersection = function () {
-  var mas = [];
-	var i = 0;
-	for(i = 0; i < arguments.length; i++) {
+  const mas = [];
+	for(let i = 0; i < arguments.length; i++) {
 		mas.push(arguments[i]);
 	}
-	var j = 0;
-	var inp = 0;
-	var mas_inter = mas[0];
+	const mas_inter = mas[0];
 	mas.shift();
 	while(mas.length > 0) {
-	 	for (i = 0; i < mas_inter.length; i++)	{
-			inp = 0;
-			for(j = 0; j < mas[0].length; j++) {
-				if(mas[0][j] === mas_inter[i]) inp = 1;
-	 			if(isNaN(mas[0][j]) == true && isNaN(mas_inter[i]) == true) inp = 1;
+	 	for (let i = 0; i < mas_inter.length; i++)	{
+			let inp = 0;
+			for(let j = 0; j < mas[0].length; j++) {
+				if(mas[0][j] === mas_inter[i]) {
+          inp = 1;
+        }
+	 			if(isNaN(mas[0][j]) && isNaN(mas_inter[i])) {
+          inp = 1;
+        }
 			}
-			if (inp == 0) {
+			if (!inp) {
 	 	 		mas_inter.splice(i, 1);
 	 	 		i--;
 			}
