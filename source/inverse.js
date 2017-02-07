6449b7a5cf836ae6c88e597d09b0a48d6f024c2e
Ступин Никита <n.stupin@corp.mail.ru>
2017-02-07 17:45:10 +0300

    fixed comparsion

--- source/inverse.js
+++ source/inverse.js
@@ -1,9 +1,9 @@
 const inverse = function (array) {
-    if (array.length == 0) {
+    if (!array.length) {
         return array;
     }
 
-    let reversed_subarray = array.splice(1, array.length - 1).reverse();
+    const reversed_subarray = array.splice(1, array.length - 1).reverse();
 
     return array.concat(reversed_subarray);
 };
