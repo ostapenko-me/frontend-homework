c3155a240493db3799f380935e695ad2d53fe2f1
Антон Семенов <antonsemenov@MacBook-Pro-Anton.local>
2017-02-08 18:08:44 +0300

    Updated gcd2 and euclid functions

--- source/euclid.js
+++ source/euclid.js
@@ -1,19 +1,9 @@
 function gcd2( n1, n2 ) {
 	
-	return ( n2 == 0 ) ? n1 : gcd2( n2, n1 % n2 );
+	return ( n2 === 0 ) ? n1 : gcd2( n2, n1 % n2 );
 }
 
-/*function euclid( n1, n2, n3 ) {
+function euclid( ...rest ) {
 
-	return gcd2( gcd2( n1, n2 ), n3 );
-}*/
-
-function euclid() {
-
-	let gcd = arguments[ 0 ];
-	for ( let i = 1; i < arguments.length; ++ i ) {
-
-		gcd = gcd2( gcd, arguments[ i ] );
-	}
-	return gcd;
+	return rest.reduce( gcd2, rest[ 0 ] );
 }
