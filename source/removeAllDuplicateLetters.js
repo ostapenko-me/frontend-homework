0955918503e23f722fbb4cbe6d247a18fd18f5ce
eafomi4ev <eafomi4ev@gmail.com>
2017-02-08 19:33:01 +0300

    Добавлена пустая строка; двойные кавычки заменены одинарными; var заменен на let

--- source/removeAllDuplicateLetters.js
+++ source/removeAllDuplicateLetters.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const removeAllDuplicateLetters = function (myString) {
-	var charSet = new Set();
-	var result = myString;
-	for(var i = 0; i < myString.length; i++) {
+	let charSet = new Set();
+	let result = myString;
+	for(let i = 0; i < myString.length; i++) {
 		if(!charSet.has(myString[i])) {
 			charSet.add(myString[i]);
 		} else {
-			result = result.replace(new RegExp(myString[i], 'gi'), "");
+			result = result.replace(new RegExp(myString[i], 'gi'), '');
 		}
 	}
 	return result;
