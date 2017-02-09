b4da4e5abef6c154d4c8799983ceaa33cee21341
KrylovS <krylov_ss@mail.ru>
2017-02-10 00:22:31 +0300

    Fixed function plain

--- source/max.js
+++ source/max.js
@@ -4,10 +4,3 @@ const max = function (numbers) {
 	return Math.max(...numbers);
 };
 
-const plain = function (arr) {
-	var plainArr = [];
-	for(var i = 0; i < arr.length; i++) {
-		plainArr = plainArr.concat(arr[i]);	
-	}
-	return plainArr;
-};
