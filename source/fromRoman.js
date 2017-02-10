c992c7384acf65df2a377a06787d442b86c95bea
server <server@server.com>
2017-02-10 17:00:31 +0300

    small format changes

--- source/fromRoman.js
+++ source/fromRoman.js
@@ -1,17 +1,28 @@
 'use strict';
 
-const numbers_dict = {"I" : 1 ,"IV" : 4 ,"V" : 5 ,"IX" : 9, "X" : 10," XL" : 40, "L" :50,"XC" : 90,"C" : 100,"CD" : 400 ,"D" : 500,"CM" : 900,"M" : 1000};
+const numbers_dict = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+};
 
-const fromRoman = function(roman){
+const fromRoman = function (roman) {
+
     roman = roman.toUpperCase();
-    let res =0;
+    let res = 0;
     let prev = 0;
-    for (let i = roman.length -1; i >= 0 ; i--){
-        if(numbers_dict[roman[i]] < prev){
+
+    for (let i = roman.length - 1; i >= 0; i--) {
+        if (numbers_dict[roman[i]] < prev) {
             res -= numbers_dict[roman[i]];
-        } else{
+        } else {
             res += numbers_dict[roman[i]];
         }
+
         prev = numbers_dict[roman[i]];
     }
     return res;
