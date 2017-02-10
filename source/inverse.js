bc29c6fc4c7a5456fb7cb5a517863ed66d723b33
yaches <@>
2017-02-10 20:25:14 +0300

    arrow function

--- source/inverse.js
+++ source/inverse.js
@@ -1,5 +1,3 @@
 'use strict';
 
-const inverse = function (elements) {
-	return [...elements].splice(0, 1).concat([...elements].splice(1).reverse());
-};
+const inverse = elements => [...elements].splice(0, 1).concat([...elements].splice(1).reverse());
