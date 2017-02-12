44f32d9808131ed484bc46dbbf64f96048fe4c17
trntga <trntga@gmail.com>
2017-02-12 22:10:33 +0300

    Add files via upload

--- source/chess.js
+++ source/chess.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const chess = function (num) {
-    if (+num < 2) return null;
+    if (+num < 2) return;
 
-    var arr = Array.apply(null, Array(+num * +num))
-        .map(function(item, i) {
-            return ((i % +num + Math.floor(i / +num)) % 2 === 1 ) ? ' ' : '*';
-        }).join('')
-        .match(new RegExp('.{1,' + +num + '}', 'g'))
-        .map(function(item) {return item + '\n'})
+    let arr = Array.apply(null, Array(+num * +num))
+        .map((item, i) => ((i % +num + Math.floor(i / +num)) % 2 === 1 ) ? ' ' : '*')
+        .join('')
+        .match(new RegExp(`.{1,${+num}}`, `g`))
+        .map(item => item + '\n')
         .join('');
     return arr;
 };
