38de146ffb5249c080904f3d7fcef9a57a2b1dda
ChernovAndrey <chernov.andrey.98@mail.ru>
2017-02-12 15:01:33 +0400

    Add files via upload

--- source/removeDuplicateLettersBegin.js
+++ source/removeDuplicateLettersBegin.js
@@ -1,37 +1,8 @@
 'use strict';
-
-QUnit.module('Тестируем функцию removeDuplicateLettersBegin', function () {
-	QUnit.test('Оставляет без изменений строки, где все символы уникальны', function (assert) {
-		assert.strictEqual(removeDuplicateLettersBegin('1234'), '1234');
-		assert.strictEqual(removeDuplicateLettersBegin('abcd'), 'abcd');
-		assert.strictEqual(removeDuplicateLettersBegin('олдж фыва'), 'олдж фыва');
-		assert.strictEqual(removeDuplicateLettersBegin(',.;=\n\t '), ',.;=\n\t ');
-	});
-
-	QUnit.test('Удаляет повторяющиеся символы, оставляя только один', function (assert) {
-		assert.strictEqual(removeDuplicateLettersBegin('1111'), '1');
-		assert.strictEqual(removeDuplicateLettersBegin('aaaa'), 'a');
-		assert.strictEqual(removeDuplicateLettersBegin('ыыыы'), 'ы');
-		assert.strictEqual(removeDuplicateLettersBegin('==== ||||'), '= |');
-	});
-
-	QUnit.test('Оставляет символ, который встречался в строке раньше всего', function (assert) {
-		assert.strictEqual(removeDuplicateLettersBegin('12111'), '12');
-		assert.strictEqual(removeDuplicateLettersBegin('qwqqq'), 'qw');
-		assert.strictEqual(removeDuplicateLettersBegin('ыцыыы'), 'ыц');
-		assert.strictEqual(removeDuplicateLettersBegin('=*=== |+|||'), '=* |+');
-	});
-
-	QUnit.test('Работает правильно на случайных строчках', function (assert) {
-		assert.strictEqual(removeDuplicateLettersBegin('мама мыла раму'), 'ма ылру');
-		assert.strictEqual(removeDuplicateLettersBegin('от топота копыт'), 'от пакы');
-		assert.strictEqual(removeDuplicateLettersBegin('hello world'), 'helo wrd');
-	});
-	//мой тест
-	QUnit.test('удаляет пары символов, оставляя только оин из них', function (assert){
-		assert.strictEqual(removeDuplicateLettersBegin('11223344'), '1234');
-		assert.strictEqual(removeDuplicateLettersBegin('aabbccddee'), 'abcde');
-		assert.strictEqual(removeDuplicateLettersBegin('ыыллаа'), 'ыла');
-		assert.strictEqual(removeDuplicateLettersBegin('==||'), '=|');
-	});
-});
+//Chernov Andrey
+function removeDuplicateLettersBegin(str)
+{
+	var strResult="";
+	for (var i=0;i<str.length;i++) if(strResult.indexOf(str.charAt(i))==-1) strResult+=str.charAt(i)	
+	return strResult;
+}
