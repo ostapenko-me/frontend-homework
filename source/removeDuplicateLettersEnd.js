ac9065884eb9ca755c2a288bbb410d38ce96376f
Shelavin Ilya <123jerry123@mail.ru>
2017-02-16 00:14:30 +0300

    Update removeDuplicateLettersEnd.js

--- source/removeDuplicateLettersEnd.js
+++ source/removeDuplicateLettersEnd.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const removeDuplicateLettersEnd = function (string) {
-	var resStr = "";
+	let resStr = "";
 
 	for (var i = 0; i < string.length; i++) {
-		var j = i + 1;
-		for (; j < string.length && string[j] != string[i]; j++);
-		if (j == string.length)
+		let j = i + 1;
+		for (; j < string.length && string[j] !== string[i]; j++);
+		if (j === string.length)
 			resStr += string[i];
 	}
 
