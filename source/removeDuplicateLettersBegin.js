687ad6cc898d3be0eef92c22e2d6111fbc20f3e8
ChernovAndrey <chernov.andrey.98@mail.ru>
2017-02-16 08:49:29 +0400

    Add files via upload

--- source/removeDuplicateLettersBegin.js
+++ source/removeDuplicateLettersBegin.js
@@ -2,7 +2,7 @@
 //Chernov Andrey
 function removeDuplicateLettersBegin(str){
 	let strResult="";
-	for (let i=0;i<str.length;i++) if(strResult.includes(str.charAt(i))===false){ 
+	for (let i=0;i<str.length;i++) if(!strResult.includes(str.charAt(i))){ 
 		strResult+=str.charAt(i)
 	}		
 	return strResult;
