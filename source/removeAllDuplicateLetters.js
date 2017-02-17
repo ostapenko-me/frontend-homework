8a9d42381c7e56d58ea612f7a567999fc64217d3
Yury <ppro100takk@mail.ru>
2017-02-17 18:52:46 +0300

    Change var for let

--- source/removeAllDuplicateLetters.js
+++ source/removeAllDuplicateLetters.js
@@ -1,7 +1,7 @@
 const removeAllDuplicateLetters = function (str) {
-	var result = str;
+	let result = str;
 	
-	for (var i = 0; i < str.length; i++) {
+	for (let i = 0; i < str.length; i++) {
 		if(~str.indexOf(str[i], i+1)){	
  	 		result = result.split(str[i]).join('');
  	 	}
