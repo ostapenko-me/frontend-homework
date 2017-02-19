e93d1ec334b3ca5426689c9b1c99075620b25c48
VyazminIV <ilja.vyazmin@mail.ru>
2017-02-19 22:00:21 +0300

    HW_0_1

--- source/sortString.js
+++ source/sortString.js
@@ -1,19 +1,14 @@
 'use strict';
 
 function sortString (string) {
-	var words = string.split(" ");
-	var chars;
-	for (var i = 0; i < words.length; i++){
+	let words = string.split(" ");
+	let chars;
+	for (let i = 0; i < words.length; i++){
 		chars = words[i].split("");
-		chars = chars.sort(function (a, b) {
-			return a.localeCompare(b);
-		});
+		chars = chars.sort( (a, b) => a.localeCompare(b) );
 		words[i] = chars.join("");
 	}
-	words = words.sort(function (a, b) {
-		return a.localeCompare(b);
-	});
-
+	words = words.sort( (a, b) => a.localeCompare(b) );
 	string = words.join(" ");
 	return string;
 }
