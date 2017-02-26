f9df74d6af9255194218a718303f0df0d26fd2d4
Pacman29 <balyakin.d.v@gmail.com>
2017-02-26 19:31:35 +0300

    правка

--- source/fromRoman.js
+++ source/fromRoman.js
@@ -3,45 +3,23 @@
  */
 'use strict';
 function fromRoman(val) {
-    let a = val.split('');
-    let b = [];
-    for(let i = 0; i<a.length; ++i){
-        switch (a[i].toLowerCase()){
-            case "i":
-                b[i] = 1;
-                break;
-            case "v":
-                b[i] = 5;
-                break;
-            case "x":
-                b[i] = 10;
-                break;
-            case "l":
-                b[i] = 50;
-                break;
-            case "c":
-                b[i] = 100;
-                break;
-            case "d":
-                b[i] = 500;
-                break;
-            case "m":
-                b[i] = 1000;
-                break;
-        }
-    }
 
-    for(let i = 0; i<b.length-1; ++i){
-        console.info(b.join());
-        if(b[i]<b[i+1]){
-            b[i+1] -= b[i];
-            b.splice(i,1);
-        }
-    }
-    console.log(b.join());
-    let result = 0;
-    b.forEach(function (iter,i,arr) {
-        result+=iter;
+    let roman = {
+        'i': 1,
+        'v': 5,
+        'x': 10,
+        'l': 50,
+        'c': 100,
+        'd': 500,
+        'm': 1000
+    };
+    let result = roman[val[0].toLowerCase()];
+    val.toLowerCase().split("").reduce((previous,current) => {
+        let p = roman[previous];
+        let c = roman[current];
+        result += (p<c) ? (c-2*p ) : (c);
+        return current;
     });
+
     return result;
 }
