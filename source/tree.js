cd6c6f67bf5aea3c2b6b77b0c52802addf52f8c7
SCaptainCAP <SGV123@yandex.ru>
2017-02-26 17:59:36 +0300

    DZ1 fixed

--- source/tree.js
+++ source/tree.js
@@ -1,24 +1,24 @@
 'use strict';
 
-const tree = function (height) {
-	if (height < 3) {
+const tree = (height) => {
+    if (height < 3) {
         return null;
     }
-	var answer = '';
-    var spaces = height - 2;
-    var stars = 1;
-	for (let i = height; i > 1; i--) {
-		answer += ' '.repeat(spaces);
-		answer += '*'.repeat(stars);
+    let answer = '';
+    let spaces = height - 2;
+    let stars = 1;
+    for (let i = height; i > 1; i--) {
         answer += ' '.repeat(spaces);
-		answer += '\n';
-		spaces -= 1;
-		stars += 2;
-	}
+        answer += '*'.repeat(stars);
+        answer += ' '.repeat(spaces);
+        answer += '\n';
+        spaces -= 1;
+        stars += 2;
+    }
     spaces = height - 2;
-	answer += ' '.repeat(spaces);
-	answer += '|';
+    answer += ' '.repeat(spaces);
+    answer += '|';
     answer += ' '.repeat(spaces);
     answer += '\n';
-	return answer;
-};
+    return answer;
+}
